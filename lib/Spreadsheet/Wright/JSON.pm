e737f7417beba75d1842175ecff069005ee2405f
Andrius Merkys <andrius.merkys@gmail.com>
2020-01-06 09:20:02 +0200

    Implementing filehandle support in Spreadsheet::Wright::JSON.

--- lib/Spreadsheet/Wright/JSON.pm
+++ lib/Spreadsheet/Wright/JSON.pm
@@ -19,8 +19,18 @@ sub new
 {
 	my ($class, %args) = @_;
 	my $self = bless { 'options' => \%args }, $class;
-	$self->{'_FILENAME'} = $args{'file'} // $args{'filename'}
-		or croak "Need filename.";
+
+	my $fh = $args{'fh'} // $args{'filehandle'};
+	if ($fh)
+	{
+		$self->{'_FH'} = $fh;
+	}
+	else
+	{
+		$self->{'_FILENAME'} = $args{'file'} // $args{'filename'}
+			or croak "Need filename";
+	}
+
 	$self->{'_WORKSHEET'} = $args{'sheet'} // 'Sheet1';
 	return $self;
 }
